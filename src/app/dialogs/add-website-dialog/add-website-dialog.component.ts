9227f0d5676e5c0aeae4d0f7677fc29a35eb6003
João Vicente <joao.vicente@campus.ul.pt>
2021-11-03 17:34:37 +0000

    Removing all the domain layer references

--- src/app/dialogs/add-website-dialog/add-website-dialog.component.ts
+++ src/app/dialogs/add-website-dialog/add-website-dialog.component.ts
@@ -91,10 +91,10 @@ export class AddWebsiteDialogComponent implements OnInit {
         [Validators.required],
         this.nameValidator.bind(this)
       ),
-      domain: new FormControl(
+      startingUrl: new FormControl(
         "",
         [Validators.required, domainValidator, domainMissingProtocol],
-        this.domainValidator.bind(this)
+        this.urlValidator.bind(this)
       ),
       declaration: new FormControl(),
       stamp: new FormControl(),
@@ -125,14 +125,15 @@ export class AddWebsiteDialogComponent implements OnInit {
       this.loadingUsers = false;
     });
 
-    this.get.listOfEntities(-1, 0, '', '', '').subscribe((entities) => {
+    this.get.listOfEntities(-1, 0, "", "", "").subscribe((entities) => {
       if (entities !== null) {
         this.entities = entities;
-        this.filteredEntities = this.websiteForm.controls.entities.valueChanges.pipe(
-          map((entity) =>
-            entity ? this.filterEntities(entity) : this.entities.slice()
-          )
-        );
+        this.filteredEntities =
+          this.websiteForm.controls.entities.valueChanges.pipe(
+            map((entity) =>
+              entity ? this.filterEntities(entity) : this.entities.slice()
+            )
+          );
       }
 
       this.loadingEntities = false;
@@ -162,7 +163,9 @@ export class AddWebsiteDialogComponent implements OnInit {
     e.preventDefault();
 
     const name = _.trim(this.websiteForm.value.name);
-    const domain = encodeURIComponent(_.trim(this.websiteForm.value.domain));
+    const startingUrl = encodeURIComponent(
+      _.trim(this.websiteForm.value.startingUrl)
+    );
     const declaration =
       this.websiteForm.value.declaration === ""
         ? null
@@ -186,7 +189,7 @@ export class AddWebsiteDialogComponent implements OnInit {
 
     const formData = {
       name,
-      domain,
+      startingUrl,
       declaration,
       declarationDate,
       stamp,
@@ -285,11 +288,11 @@ export class AddWebsiteDialogComponent implements OnInit {
     }
   }
 
-  domainValidator(control: AbstractControl): Observable<any> {
-    const domain = _.trim(control.value);
+  urlValidator(control: AbstractControl): Observable<any> {
+    const url = _.trim(control.value);
 
-    if (domain !== "") {
-      return this.verify.domainExists(domain);
+    if (url !== "") {
+      return this.verify.websiteUrlExists(url);
     } else {
       return null;
     }
