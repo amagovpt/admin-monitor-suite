58f83bda683bbfd7269d26f35601282464977250
Estriga97 <fc47839@alunos.fc.ul.pt>
2023-10-25 15:25:25 +0100

    Update edit-website-dialog.component.ts

--- src/app/dialogs/edit-website-dialog/edit-website-dialog.component.ts
+++ src/app/dialogs/edit-website-dialog/edit-website-dialog.component.ts
@@ -199,7 +199,7 @@ export class EditWebsiteDialogComponent implements OnInit {
 
   setDefault(): void {
     this.websiteForm.controls.name.setValue(this.defaultWebsite.Name);
-    this.websiteForm.controls.url.setValue(this.defaultWebsite.StartingUrl);
+    this.websiteForm.controls.startingUrl.setValue(this.defaultWebsite.StartingUrl);
     this.websiteForm.controls.declaration.setValue(
       this.defaultWebsite.Declaration
     );
